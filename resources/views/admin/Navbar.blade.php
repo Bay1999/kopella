22fd0ee9de9e9facf34079855a80471df270e60f
Bay1999 <bayuagung1074@gmail.com>
2019-12-26 11:27:41 +0700

    Update Navbar.blade.php

--- resources/views/admin/Navbar.blade.php
+++ resources/views/admin/Navbar.blade.php
@@ -53,24 +53,7 @@
 		  	<p>Ini adalah Home</p>
 		  </div>
 	    </li>
-		<!-- <li class="item">
-	      <a class="link" href="#">Pegawai</a>
-	    </li>
-		<li class="item">
-	      <a class="link" href="#">Barang</a>
-	    </li> -->
-
-	    <!-- Dropdown -->
-	    <!-- <li class="nav-item dropdown">
-	      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-	        Dropdown link
-	      </a>
-	      <div class="dropdown-menu">
-	        <a class="dropdown-item" href="#">Link 1</a>
-	        <a class="dropdown-item" href="#">Link 2</a>
-	        <a class="dropdown-item" href="#">Link 3</a>
-	      </div>
-	    </li> -->
+		
 	  </ul>
   </div>
 </nav>
